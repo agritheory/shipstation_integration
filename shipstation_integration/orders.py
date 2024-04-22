d8b0fa71abd2a9542d3711e854d334b18efe2e59
Tyler Matteson <tyler@agritheory.com>
2024-04-22 17:15:12 -0400

    feat: integrate sales partner, handle tax withholding

--- shipstation_integration/orders.py
+++ shipstation_integration/orders.py
@@ -151,6 +151,8 @@ def create_erpnext_order(order: "ShipStationOrder", store: "ShipstationStore") -
 			"has_pii": True,
 		}
 	)
+	if store.sales_partner:
+		so.sales_partner = store.sales_partner
 
 	if store.get("is_amazon_store"):
 		update_hook = frappe.get_hooks("update_shipstation_amazon_order")
@@ -216,6 +218,18 @@ def create_erpnext_order(order: "ShipStationOrder", store: "ShipstationStore") -
 				"cost_center": store.cost_center,
 			},
 		)
+	if order.tax_amount and store.withholding:
+		# reverse withholding
+		so.append(
+			"taxes",
+			{
+				"charge_type": "Actual",
+				"account_head": store.tax_account,
+				"description": "Shipstation Tax Amount",
+				"tax_amount": order.tax_amount * -1,
+				"cost_center": store.cost_center,
+			},
+		)
 
 	if order.shipping_amount:
 		so.append(
@@ -237,12 +251,14 @@ def create_erpnext_order(order: "ShipStationOrder", store: "ShipstationStore") -
 
 	before_submit_hook = frappe.get_hooks("update_shipstation_order_before_submit")
 	if before_submit_hook:
-		so = frappe.get_attr(before_submit_hook[0])(store, so)
-		so.save()
+		so = frappe.get_attr(before_submit_hook[0])(store, so, order)
+		if so:
+			so.save()
 
-	so.submit()
-	frappe.db.commit()
-	return so.name
+	if so:
+		so.submit()
+		frappe.db.commit()
+		return so.name
 
 
 def get_item_notes(item: "ShipStationOrderItem"):
