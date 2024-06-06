b0b43fb0e07efda0a30254b7e7147c352ec108db
fproldan <franciscoproldan@gmail.com>
2024-06-06 12:30:33 +0000

    feat: helptext, control

--- shipstation_integration/shipstation_integration/overrides/sales_order.py
+++ shipstation_integration/shipstation_integration/overrides/sales_order.py
@@ -4,5 +4,7 @@ from erpnext.selling.doctype.sales_order.sales_order import SalesOrder
 
 class ShipStationSalesOrder(SalesOrder):
 	def calculate_commission(self):
-		if not frappe.db.get_value("Sales Partner", self.sales_partner, "commission_formula"):
+		if not self.shipstation_order_id or not frappe.db.get_value(
+			"Sales Partner", self.sales_partner, "commission_formula"
+		):
 			super().calculate_commission()
