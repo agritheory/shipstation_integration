83cf38185e675258b191a0f85997ea32093486d4
Kevin Shenk <mail@avu.nu>
2024-11-13 15:06:15 +0000

    fix: paid amount handling on refunded orders

--- shipstation_integration/orders.py
+++ shipstation_integration/orders.py
@@ -233,7 +233,7 @@ def create_erpnext_order(
 	if store.customer:
 		so.customer_name = order.customer_email
 	# coupons
-	if order.amount_paid and Decimal(so.grand_total).quantize(Decimal(".01")) != order.amount_paid:
+	if order.get("amount_paid", 0) > 0 and Decimal(so.grand_total).quantize(Decimal(".01")) != order.amount_paid:
 		difference_amount = Decimal(Decimal(so.grand_total).quantize(Decimal(".01")) - order.amount_paid)
 		so.shipstation_discount = difference_amount
 		account = store.difference_account
