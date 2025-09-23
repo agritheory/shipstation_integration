c6cf5c3432f771de2505e5aca107d3bf3354fe7b
Tyler Matteson <tyler@agritheory.com>
2025-09-23 09:45:40 -0400

    cust: clean up customizations

--- shipstation_integration/public/js/shipping.js
+++ shipstation_integration/public/js/shipping.js
@@ -1,3 +1,6 @@
+// Copyright (c) 2025, AgriTheory and contributors
+// For license information, please see license.txt
+
 if (!window.shipping) window.shipping = {}
 
 shipping.shipstation = frm => {
