020ebbd2a1fbf07f8e01b7b2fedb86f9e3fc7eb8
lauty95 <lautaroj95@gmail.com>
2026-05-21 13:34:18 +0000

    fix: hide new rows and sections in new records

--- shipstation_integration/shipstation_integration/doctype/tracking_number/tracking_number.js
+++ shipstation_integration/shipstation_integration/doctype/tracking_number/tracking_number.js
@@ -1,8 +1,10 @@
 // Copyright (c) 2026, AgriTheory and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Tracking Number", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on('Tracking Number', {
+	refresh(frm) {
+		frm.set_df_property('tracking_number_event', 'cannot_add_rows', true)
+		frm.toggle_display('section_tracking_number_event', !frm.is_new())
+		frm.toggle_display('section_map', !frm.is_new())
+	},
+})
