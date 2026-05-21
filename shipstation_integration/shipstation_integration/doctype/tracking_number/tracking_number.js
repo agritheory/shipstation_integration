80c08540d0b603ac995e2e96980180ba316cfeb4
lauty95 <lautaroj95@gmail.com>
2026-05-21 14:49:36 +0000

    fix: hide sections when dt is submitted

--- shipstation_integration/shipstation_integration/doctype/tracking_number/tracking_number.js
+++ shipstation_integration/shipstation_integration/doctype/tracking_number/tracking_number.js
@@ -4,7 +4,7 @@
 frappe.ui.form.on('Tracking Number', {
 	refresh(frm) {
 		frm.set_df_property('tracking_number_event', 'cannot_add_rows', true)
-		frm.toggle_display('section_tracking_number_event', !frm.is_new())
-		frm.toggle_display('section_map', !frm.is_new())
+		frm.toggle_display('section_tracking_number_event', frm.doc.docstatus)
+		frm.toggle_display('section_map', frm.doc.docstatus)
 	},
 })
